0993acf9ce1689c37301869536aa14731d4d51e4
HP <ularezephaniah@gmail.com>
2024-11-04 20:29:50 +0300

    import-export

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import {username, city }from '../data/user'
+
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        Liza is a Web Developer from New York
       </h1>
     </div>
   );
 }
+
+export default Home;
